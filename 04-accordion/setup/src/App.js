c7707a200704d60f34c43d3c9c53677d804f2915
Divano-home <dusanivanovic@mail.com>
2023-02-01 11:45:32 +0100

    project accordions - DONE

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,36 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import questions from "./data";
+import data from "./data";
+import Question from "./Question";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data); // not needed, could just use const questions = data
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
+
+/* return (
+  <main>
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
+    </div>
+  </main>
+); */
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}{" "}
+      {/* if showInfo is true, than show <p>info</p> */}
+    </article>
+  );
 };
 
 export default Question;
+
+/* 
+  return questions.map((question) => {
+    const { id, title, info } = question;
+    return (
+      <div className="question" key={id}>
+        <header>
+          <h4 className="title">{title}</h4>
+          <button className="btn">
+            <AiOutlinePlus />
+          </button>
+        </header>
+        <p>{info}</p>
+      </div>
+    );
+  }); */
